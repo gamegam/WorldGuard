c04eed670341bf897df5a488c87a97907eb34dc9
gamegam <48847058+gamegam@users.noreply.github.com>
2024-02-26 23:30:46 +0900

    Add files via upload

---
+++ src/gamegam/WorldGuardPlugin/Type.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace gamegam\WorldGuardPlugin;
+
+class Type{
+
+	public array $array = [
+		"build" => "build",
+		"tnt" => "tnt",
+		"invincible" => "invincible",
+		"lava-flow" => "lava-flow",
+		"use" => "use",
+		"water" => "water",
+		"tnt-damage" => "tnt-damage",
+		"pvp" => "pvp",
+		"mob-damage" => "mob-damage",
+		"mob-pvp" => "mob-pvp",
+		"fire" => "fire",
+	];
+
+	public function isType($name): bool{
+		$bool = false;
+		if(isset($this->array[$name])){
+			$bool = true;
+		}
+		return $bool;
+	}
+}
