0a492d56a6b2dc3d558aede7bd0498ae31a637ff
gamegam <48847058+gamegam@users.noreply.github.com>
2024-02-26 22:15:05 +0900

    Add files via upload

---
+++ src/gamegam/WorldGuard/Language/ConfigGuard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace gamegam\WorldGuard\Language;
+
+class ConfigGuard{
+
+	public const ingame = "ingame";
+	public const HELP = "help";
+	public const isMode = "isMode";
+	public const position = "pos";
+}
